5709bee6ecced2d529b57a5a31412dff149008ea
arthur <thursilverio@gmail.com>
2022-08-21 16:11:43 -0300

    It is a weak file header, i know it is poor, I will make it better one day

--- File.h
+++ File.h
@@ -2,15 +2,18 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <iostream>
+#include <cstdlib>
+
 /* There is no bounding for this structure */
 struct FileHeader {
-    short int __SizeOfBlocks;
-    long long int __OffsetToInventoryBlock;
+    char MAGIC_SING[11] = ".AACMarket";
     long long int __OffsetToShelfBlock;
-    int __numberOfSections;
-    int __numberOfShelfsOnSection;
-    long int __SizeOfBuffer;
-}fileHeader, FILEHEADER, fileheader;
+    long long int __OffsetToInventoryBlock;
+    int __numberOfSections = 0;
+    int __numberOfShelfsOnSection = 0;
+    int __numberOfProductsOnShelf = 0;
+};
 
 
-#endif !FILE_H
+#endif // !FILE_H
