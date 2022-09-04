2bf95652bb57a3ff3283c0bcafa4a11c67e74014
arthur <thursilverio@gmail.com>
2022-09-04 12:45:20 -0300

    Create definition for the member functions of Product class

--- Product.h
+++ Product.h
@@ -8,36 +8,148 @@
 #include <cstring>
 #include <ctime>
 
-typedef unsigned long long int rrn_t;
+#define NAME_SIZE 20
+#define DESCRIPTION_SIZE 80
+#define BRAND_SIZE 10
 
 class Product {
 private:
-	rrn_t mRrn;
-	char mName[20];
-	char mDescription[80];
-	char mBrand[10];
+	char mName[NAME_SIZE];
+	char mDescription[DESCRIPTION_SIZE];
+	char mBrand[BRAND_SIZE];
 	time_t mExpirationDate;
 	time_t mManufacturingDate;
-	time_t AdditionDate;
-	unsigned int Lote;
+	time_t mAdditionDate;
+	unsigned int mLote;
 	// BarCode _barcode; /* Stands for computer vision I think */
 public:
-	Product();
+	Product(char Name[], char Description[], 
+		char Brand[], time_t ExpirationDate, time_t ManufacturingDate, 
+			time_t AdditionDate, unsigned int Lote);
+
 	~Product();
+
 	const char* getName();
-	void setName(const char* name);
+
+	void setName(const char* Name);
+
 	const char* getDescription();
-	void setDescription(const char* description);
+
+	void setDescription(const char* Description);
+
 	const char* getBrand();
-	void setBrand(const char* brand);
+
+	void setBrand(const char* Brand);
+
 	time_t getExpirationDate();
-	void setExpirationDate(time_t expiration_date);
+
+	void setExpirationDate(time_t ExpirationDate);
+
 	time_t getManufacturingDate();
-	void setManufacturingDate(time_t manufacturing_date);
+
+	void setManufacturingDate(time_t ManufacturingDate);
+
 	time_t getAdditionDate();
-	void setAdditionDate(time_t adding_date);
+
+	void setAdditionDate(time_t AdditionDate);
+
 	unsigned int getLote();
-	void setLote(unsigned int lote);
+
+	void setLote(unsigned int Lote);
 };
+
+Product::Product (
+char Name[], 
+char Description[], 
+char Brand[], 
+time_t ExpirationDate, 
+time_t ManufacturingDate, 
+time_t AdditionDate, 
+unsigned int Lote
+)
+{
+	memcpy(mName, Name, NAME_SIZE);
+	memcpy(mDescription, Description, DESCRIPTION_SIZE);
+	memcpy(mBrand, Brand, BRAND_SIZE);
+	mExpirationDate = ExpirationDate;
+	mManufacturingDate = ManufacturingDate;
+	time_t currTime; mAdditionDate = time(&currTime);
+	mLote = Lote;
+}
+
+Product::~Product()
+{
+	std::cerr << "WARING RING 5 : Destructing the object" << std::endl;
+}
+
+const char* Product::getName()
+{
+	return mName;
+}
+
+void Product::setName(const char* Name)
+{
+	memcpy(mName, Name, NAME_SIZE);
+}
+
+const char* Product::getDescription()
+{
+	return mDescription;
+}
+
+void Product::setDescription(const char* Description)
+{
+	memcpy(mDescription, Description, DESCRIPTION_SIZE);
+}
+
+const char* Product::getBrand()
+{
+	return mBrand;
+}
+
+void Product::setBrand(const char* Brand)
+{
+	memcpy(mBrand, Brand, BRAND_SIZE);
+}
+
+time_t Product::getExpirationDate()
+{
+	return mExpirationDate;
+}
+
+void Product::setExpirationDate(time_t ExpirationDate)
+{
+	mExpirationDate = ExpirationDate;
+}
+
+time_t Product::getManufacturingDate()
+{
+	return mManufacturingDate;
+}
+
+void Product::setManufacturingDate(time_t ManufacturingDate)
+{
+	mManufacturingDate = ManufacturingDate;
+}
+
+time_t Product::getAdditionDate()
+{
+	return mAdditionDate;
+}
+
+void Product::setAdditionDate(time_t AdditionDate)
+{
+	mAdditionDate = AdditionDate;
+}
+
+unsigned int Product::getLote()
+{
+	return mLote;
+}
+
+void Product::setLote(unsigned int Lote)
+{
+	mLote = Lote;
+}
 
 #endif !PRODUCT_H
