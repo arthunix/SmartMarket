8ea4901427f056978253709f40d0400a41e82b86
arthur <thursilverio@gmail.com>
2022-09-09 12:18:24 -0300

    Add some member functions

--- Market.h
+++ Market.h
@@ -10,6 +10,17 @@ class Market {
 private:
 	IO<TNumOfSections,TNumOfShelfsOnSection,TNumOfProductsOnShelf> mIOfs;
 	Index mIndex;
+
+public:
+	Market();
+	~Market();
+
+	list* LookupForAProducts(char name[]);
+
+	Product getProduct(structio_t whereIs);
+	void changeProduct(structio_t whereIs);
+	void insertProduct(Product toInsert);
+	void removeProduct(structio_t whereIsToRemove);
 };
 
 
