40992dd04d344afeafcfc8f322e6346e9c1c09fa
arthur <thursilverio@gmail.com>
2022-09-23 18:39:51 -0300

    log io operation diretives

--- IO.h
+++ IO.h
@@ -13,11 +13,36 @@
 
 #define FILENAME "SMARTMARKET"
 
-#define log_file_operations_error(error) {                                  \
-if (err != 0)                                                               \
-    std::cout << "Fail in file operations -> ";                             \
-    std::cout << "error code = " << error << std::endl;                     \
+#ifndef _DEBUG
+#define LOG_fseek_call(x) x;
+#define LOG_fwrite_call(x) x;
+#define LOG_fread_call(x) x;
+#define LOG_fopen_call(x) x;
+#else
+
+#define LOG_fopen_call(x) {                                                                                                     \
+    err = x;                                                                                                                    \
+    if(err == NULL) fprintf(stderr, "ERROR at: %s on line: %i \n", #x, __LINE__);                                               \
 }
+
+#define LOG_fseek_call(x) {                                                                                                     \
+    err = x;                                                                                                                    \
+    if(err != 0) fprintf(stderr, "ERROR at: %s on line: %i \n", #x, __LINE__);                                                  \
+}
+
+#define LOG_fwrite_call(x) {                                                                                                    \
+    err = x;                                                                                                                    \
+    if(err == 0) fprintf(stderr, "ERROR writing at: %s, %i bytes written, on line: %li \n", #x, __LINE__);                      \
+    else fprintf(stderr, "Writing at: %s, %i bytes written, on line: %i\n", #x, __LINE__);                                      \
+}
+
+#define LOG_fread_call(x) {                                                                                                     \
+    err = x;                                                                                                                    \
+    if(err == 0) fprintf(stderr, "ERROR reading at: %s, %i bytes read, on line: %li \n", #x, __LINE__);                         \
+    else fprintf(stderr, "Reading at: %s, %i bytes read, on line: %li \n", #x, __LINE__);                                    \
+}
+
+#endif
 
 struct product_storage_t {
     bool is_deleted = false;
@@ -33,66 +58,49 @@ private:
     FILE* mIoFileStream;
     FileHeader mFileHeader;
     product_controller_t mMtf[TNumOfSections][TNumOfShelfsOnSection][TNumOfProductsOnShelf];
-    errno_t err;
 
+    errno_t err;
     int gNum = 0;
 
     void ReadFileHeader()
     {
         long int pos = ftell(mIoFileStream);
-        err = fseek(mIoFileStream, 0, SEEK_SET);
-        log_file_operations_error(err);
-        err = fread((char*)&mFileHeader, sizeof(FileHeader), 1, mIoFileStream);
-        log_file_operations_error(err);
-        err = fseek(mIoFileStream, pos, SEEK_SET);
-        log_file_operations_error(err);
+        LOG_fseek_call(fseek(mIoFileStream, 0, SEEK_SET));
+        LOG_fread_call(fread((char*)&mFileHeader, sizeof(FileHeader), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, pos, SEEK_SET));
     }
 
     void WriteFileHeader()
     {
         long int pos = ftell(mIoFileStream);
-        err = fseek(mIoFileStream, 0, SEEK_SET);
-        log_file_operations_error(err);
-        err = fwrite((char*)&mFileHeader, sizeof(FileHeader), 1, mIoFileStream);
-        log_file_operations_error(err);
-        err = fseek(mIoFileStream, pos, SEEK_SET);
-        log_file_operations_error(err);
+        LOG_fseek_call(fseek(mIoFileStream, 0, SEEK_SET));
+        LOG_fwrite_call(fwrite((char*)&mFileHeader, sizeof(FileHeader), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, pos, SEEK_SET));
     }
 
     void ReadFileMtf()
     {
         long int pos = ftell(mIoFileStream);
-        err = fseek(mIoFileStream, mFileHeader.OffsetToMtf, SEEK_SET);
-        log_file_operations_error(err);
-        err = fread((char*)&mMtf, sizeof(mMtf), 1, mIoFileStream);
-        log_file_operations_error(err);
-        err = fseek(mIoFileStream, pos, SEEK_SET);
-        log_file_operations_error(err);
+        LOG_fseek_call(fseek(mIoFileStream, mFileHeader.OffsetToMtf, SEEK_SET));
+        LOG_fread_call(fread((char*)&mMtf, sizeof(mMtf), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, pos, SEEK_SET));
     }
 
     void WriteFileMtf()
     {
         long int pos = ftell(mIoFileStream);
-        err = fseek(mIoFileStream, mFileHeader.OffsetToMtf, SEEK_SET);
-        log_file_operations_error(err);
-        err = fwrite((char*)&mMtf, sizeof(mMtf), 1, mIoFileStream);
-        log_file_operations_error(err);
-        err = fseek(mIoFileStream, pos, SEEK_SET);
-        log_file_operations_error(err);
+        LOG_fseek_call(fseek(mIoFileStream, mFileHeader.OffsetToMtf, SEEK_SET));
+        LOG_fwrite_call(fwrite((char*)&mMtf, sizeof(mMtf), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, pos, SEEK_SET));
     }
 public:
     IO();
     ~IO();
 
-    void PrintFileHeader()
-    {
-        ReadFileHeader();
-        mFileHeader.printFileHeader();
-    }
-
     long long int InsertToShelf(Product product)
     {
-        long long int ret = -1; product_controller_t loop_mtf;
+        long long int ret = -1;
+        product_controller_t loop_mtf;
         unsigned short int idepth, ishelf, iproducts;
         ReadFileHeader(); ReadFileMtf();
 
@@ -115,16 +123,10 @@ public:
         product_storage_t new_inserted_product = { false, idepth, ishelf, product };
 
         ret = ftell(mIoFileStream);
-#ifdef _DEBUG
-        std::cout << "byte I will write the product = " << ret << std::endl;
-#endif // _DEBUG
+        LOG_fwrite_call(fwrite((char*)&new_inserted_product, sizeof(product_storage_t), 1, mIoFileStream));
 
-        err = fwrite((char*)&new_inserted_product, sizeof(product_storage_t), 1, mIoFileStream);
-
-        log_file_operations_error(err);
         product_controller_t correct_mtf = { true , false , ret };
         mMtf[idepth][ishelf][iproducts] = correct_mtf;
-
         mFileHeader.OffsetToShelfBlockNextFree = ftell(mIoFileStream);
 
         WriteFileHeader(); WriteFileMtf();
@@ -136,19 +138,14 @@ public:
         long whereWasI = ftell(mIoFileStream);
         product_storage_t lReadProductStructure;
 
-        err = fseek(mIoFileStream, byteoffset_to_product, SEEK_SET);
-        log_file_operations_error(err);
-        fread((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream);
+        LOG_fseek_call(fseek(mIoFileStream, byteoffset_to_product, SEEK_SET));
+        LOG_fread_call(fread((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream));
 
         lReadProductStructure.is_deleted = true;
 
-        err = fseek(mIoFileStream, byteoffset_to_product, SEEK_SET);
-        log_file_operations_error(err);
-        err = fwrite((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream);
-        log_file_operations_error(err);
-
-        err = fseek(mIoFileStream, whereWasI, SEEK_SET);
-        log_file_operations_error(err);
+        LOG_fseek_call(fseek(mIoFileStream, byteoffset_to_product, SEEK_SET));
+        LOG_fwrite_call(fwrite((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, whereWasI, SEEK_SET));
     }
 
     long long int ModifyFromShelf(long byteoffset_to_product, Product modified_product)
@@ -162,44 +159,45 @@ public:
         long whereWasI = ftell(mIoFileStream);
         product_storage_t lReadProductStructure;
 
-        err = fseek(mIoFileStream, byteoffset_to_product, SEEK_SET);
-        log_file_operations_error(err);
-        fread((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream);
-
-        err = fseek(mIoFileStream, whereWasI, SEEK_SET);
-        log_file_operations_error(err);
+        LOG_fseek_call(fseek(mIoFileStream, byteoffset_to_product, SEEK_SET));
+        LOG_fread_call(fread((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, whereWasI, SEEK_SET));
 
         return lReadProductStructure.product_itself;
     }
 
     void CleanFile();
 
-    FileHeader getFileHeader() { return mFileHeader; };
-
 #ifdef _DEBUG
     void printProducts()
     {
-        std::cout << " \n\n--- PRINTING PRODUCTS --- " << gNum << std::endl;
-        ReadFileHeader();
-        err = fseek(mIoFileStream, mFileHeader.OffsetToShelfBlock, SEEK_SET);
-        log_file_operations_error(err);
         long tell = ftell(mIoFileStream);
-        std::cout << "fteel in print products = " << tell << std::endl;
         product_storage_t lReadProductStructure;
         Product lReadProduct;
 
+        std::cout << " \n\n\n--- PRINTING PRODUCTS AT SHELFS --- " << gNum << std::endl;
+        ReadFileHeader();
+
+        LOG_fseek_call(fseek(mIoFileStream, mFileHeader.OffsetToShelfBlock, SEEK_SET));
+
         for (int k = 0; k < gNum; k++)
         {
-            std::cout << "val = " << k;
-            long tell = ftell(mIoFileStream);
-            std::cout << " fteel in print products = " << tell << std::endl;
-            fread((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream);
+            std::cout << "\n";
+            tell = ftell(mIoFileStream);
+            std::cout << " PRINTING PRODUCT NUMBER : " << k << " OFFSET : " << tell << std::endl;
+            LOG_fread_call(fread((char*)&lReadProductStructure, sizeof(product_storage_t), 1, mIoFileStream));
             lReadProduct = lReadProductStructure.product_itself;
             std::cout << "IS PRODUCT DELETED ??? = " << lReadProductStructure.is_deleted << std::endl;
             lReadProduct.printProduct();
             std::cout << "\n";
         }    
-        std::cout << " --- END OF PRINTING PRODUCTS ---\n\n " << std::endl;
+        std::cout << " --- END OF PRINTING PRODUCTS ---\n\n\n " << std::endl;
+    }
+
+    void PrintFileHeader()
+    {
+        ReadFileHeader();
+        mFileHeader.printFileHeader();
     }
 #endif // _DEBUG
 };
@@ -217,16 +215,13 @@ inline IO<TNumOfSections, TNumOfShelfsOnSection, TNumOfProductsOnShelf>::IO()
     else
     {
         std::cout << "We have no such file, writing one by the way" << std::endl;
-        err = fopen_s(&mIoFileStream, FILENAME, "w+");
-        log_file_operations_error(err);
-        err = fseek(mIoFileStream, 0, SEEK_SET);
-        log_file_operations_error(err);
+
+        LOG_fopen_call(fopen_s(&mIoFileStream, FILENAME, "w+"));
+        LOG_fseek_call(fseek(mIoFileStream, 0, SEEK_SET));
         long long int actual_byte_offset_for_fileheader = ftell(mIoFileStream);
-        err = fwrite((char*)&mFileHeader, sizeof(mFileHeader), 1, mIoFileStream);
-        log_file_operations_error(err);
+        LOG_fwrite_call(fwrite((char*)&mFileHeader, sizeof(mFileHeader), 1, mIoFileStream));
         long long int actual_byte_offset_for_mtf = ftell(mIoFileStream);
-        err = fwrite((char*)&mMtf, sizeof(mMtf), 1, mIoFileStream);
-        log_file_operations_error(err);
+        LOG_fwrite_call(fwrite((char*)&mMtf, sizeof(mMtf), 1, mIoFileStream));
         long long int actual_byte_offset_for_shelf = sizeof(mFileHeader) + sizeof(mMtf);
 
 #ifdef _DEBUG
@@ -249,32 +244,16 @@ inline IO<TNumOfSections, TNumOfShelfsOnSection, TNumOfProductsOnShelf>::IO()
         };
 
         long long int pos = ftell(mIoFileStream);
-#ifdef _DEBUG
-        std::cout << "byte offset on creation = " << (long long int)pos << std::endl;
-#endif // _DEBUG
-        err = fseek(mIoFileStream, 0, SEEK_SET);
-        log_file_operations_error(err);
-        err = fwrite((char*)&mFileHeader, sizeof(mFileHeader), 1, mIoFileStream);
-        log_file_operations_error(err);
-        err = fseek(mIoFileStream, pos,  SEEK_SET);
-        log_file_operations_error(err);
-#ifdef _DEBUG
-        std::cout << "byte offset on creation = " << (long long int)pos << std::endl;
-#endif // _DEBUG
 
-#ifdef _DEBUG
-        PrintFileHeader();
-#endif // _DEBUG
+        LOG_fseek_call(fseek(mIoFileStream, 0, SEEK_SET));
+        LOG_fwrite_call(fwrite((char*)&mFileHeader, sizeof(mFileHeader), 1, mIoFileStream));
+        LOG_fseek_call(fseek(mIoFileStream, pos,  SEEK_SET));
+
         fclose(mIoFileStream);
+
         std::cout << " --- Opening file both for reading and writing --- " << std::endl;
-        err = fopen_s(&mIoFileStream, FILENAME, "r+");
-        log_file_operations_error(err);
-#ifdef _DEBUG
-        err = fseek(mIoFileStream, 0, SEEK_END);
-        long tell = ftell(mIoFileStream);
-        log_file_operations_error(err);
-        std::cout << "fteel in at final of create = " << tell << std::endl;
-#endif // _DEBUG
+        LOG_fopen_call(fopen_s(&mIoFileStream, FILENAME, "r+"));
+        LOG_fseek_call(fseek(mIoFileStream, 0, SEEK_END));
     }
 }
 
